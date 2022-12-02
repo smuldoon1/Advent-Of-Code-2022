c3f4fc46f3cd78daeecd2054b2d0aaa7c21afba5
seanmul10 <36199832+smuldoon1@users.noreply.github.com>
2022-12-02 02:10:42 +0000

    Part 2

--- C-Sharp-Solutions/Day1.cs
+++ C-Sharp-Solutions/Day1.cs
@@ -1,10 +1,17 @@
-﻿//Day 1 - 18m 06s 17
+﻿// Part 1 - 18m 06s 17
+// Part 2 - 27m 13s 44
 class Day1
 {
     static void Main()
     {
         string[] input = File.ReadAllLines(@"F:\Advent-Of-Code-2022\C-Sharp-Solutions\Day1.txt");
-        int maxCalories = -1;
+        PartOne(input);
+        PartTwo(input);
+    }
+
+    static void PartOne(string[] input)
+    {
+        int maxCalories = 0;
         int currentTotal = 0;
         foreach (string line in input)
         {
@@ -16,6 +23,34 @@ class Day1
             }
             currentTotal += int.Parse(line);
         }
-        Console.WriteLine($"Max calories: {maxCalories}");
+        maxCalories = Math.Max(currentTotal, maxCalories);
+
+        Console.WriteLine($"Part 1: { maxCalories }");
+    }
+
+    static void PartTwo(string[] input)
+    {
+        int[] maxCalories = new int[3];
+        int currentTotal = 0;
+        foreach (string line in input)
+        {
+            if (string.IsNullOrWhiteSpace(line))
+            {
+                SetTopValues(ref maxCalories, currentTotal);
+                currentTotal = 0;
+                continue;
+            }
+            currentTotal += int.Parse(line);
+        }
+        SetTopValues(ref maxCalories, currentTotal);
+
+        Console.WriteLine($"Part 2: { maxCalories.Sum() }");
+    }
+
+    static void SetTopValues(ref int[] maxCalories, int count)
+    {
+        if (count > maxCalories[0])
+            maxCalories[0] = count;
+        Array.Sort(maxCalories);
     }
 }
