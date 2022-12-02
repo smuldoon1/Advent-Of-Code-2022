8c5adfe47e1d0efd94f6ddb5904d2af8d8fd4e71
seanmul10 <36199832+smuldoon1@users.noreply.github.com>
2022-12-02 00:45:57 +0000

    Day 1 Part 1

---
+++ C-Sharp-Solutions/Day1.cs
@@ -0,0 +1,21 @@
+﻿//Day 1 - 18m 06s 17
+class Day1
+{
+    static void Main()
+    {
+        string[] input = File.ReadAllLines(@"F:\Advent-Of-Code-2022\C-Sharp-Solutions\Day1.txt");
+        int maxCalories = -1;
+        int currentTotal = 0;
+        foreach (string line in input)
+        {
+            if (string.IsNullOrWhiteSpace(line))
+            {
+                maxCalories = Math.Max(currentTotal, maxCalories);
+                currentTotal = 0;
+                continue;
+            }
+            currentTotal += int.Parse(line);
+        }
+        Console.WriteLine($"Max calories: {maxCalories}");
+    }
+}
