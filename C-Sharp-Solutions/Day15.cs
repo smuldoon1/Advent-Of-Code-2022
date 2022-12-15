280040062b3ca5da80803c0df2fc44e463ab1daa
seanmul10 <36199832+smuldoon1@users.noreply.github.com>
2022-12-15 09:43:57 +0000

    Day 15 part 2

--- C-Sharp-Solutions/Day15.cs
+++ C-Sharp-Solutions/Day15.cs
@@ -31,7 +31,39 @@ class Day15 : Day
 
     public override string PartTwo(string[] input)
     {
-        return "";
+        var sensors = new List<Sensor>();
+        foreach (string line in input)
+        {
+            var sensor = new Sensor(Utils.GetInts(line));
+            sensors.Add(sensor);
+        }
+        var positions = new List<(int x, int y)>();
+        foreach (var sensor in sensors)
+        {
+            positions.AddRange(sensor.PositionsOutsideRadius());
+        }
+        foreach (var position in positions)
+        {
+            if (IsDistressBeacon(position.x, position.y, sensors))
+            {
+                return (position.x * 4000000L + position.y).ToString();
+            }
+        }
+        return "-1";
+    }
+
+    private static bool IsDistressBeacon(int x, int y, List<Sensor> sensors)
+    {
+        foreach (var sensor in sensors)
+        {
+            var distance = Math.Abs(x - sensor.X) + Math.Abs(y - sensor.Y);
+            if (distance <= sensor.Distance ||
+                x < 0 || y < 0 || x > 4000000 || y > 4000000)// || (x == 3859433 && y == 3323143) || (x == 2911363 && y == 2855041))
+            {
+                return false;
+            }
+        }
+        return true;
     }
 
     private class Sensor
@@ -53,16 +85,30 @@ class Day15 : Day
 
         public List<int> GetBeaconCoverage(int row)
         {
-            var xPoints = new List<int>();
+            var ints = new List<int>();
             for (int x = -1000000; x < 5000000; x++)
             {
                 int distance = Math.Abs(x - X) + Math.Abs(row - Y);
                 if (distance <= Distance)
                 {
-                    xPoints.Add(x);
+                    ints.Add(x);
                 }
             }
-            return xPoints;
+            return ints;
+        }
+
+        public List<(int, int)> PositionsOutsideRadius()
+        {
+            var points = new List<(int, int)>();
+            for (int y = -Distance; y < Distance + 1; y++)
+            {
+                int x = Math.Abs(y) - Distance - 1;
+                points.Add((X - x, Y - y));
+                points.Add((X + x, Y - y));
+            }
+            points.Add((X, Y - Distance - 1));
+            points.Add((X, Y + Distance + 1));
+            return points;
         }
     }
 }
