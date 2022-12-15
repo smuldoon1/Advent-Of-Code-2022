cd6480e9aa79bc5e47ee52ce3c95b76378cdc44a
seanmul10 <36199832+smuldoon1@users.noreply.github.com>
2022-12-15 07:32:43 +0000

    Negative ints

--- C-Sharp-Solutions/Utils.cs
+++ C-Sharp-Solutions/Utils.cs
@@ -4,7 +4,7 @@ public static class Utils
 {
     public static int Mod(int a, int b) => ((a % b) + b) % b;
 
-    public static int[] GetInts(string line) => Regex.Split(line, @"\D+").Skip(1).Select(x => int.Parse(x)).ToArray();
+    public static int[] GetInts(string line) => Regex.Split(line, @"[^0-9-]+").Skip(1).Select(x => int.Parse(x)).ToArray();
 
-    public static int GetInt(string line) => int.Parse(Regex.Match(line, @"\d+").Value);
+    public static int GetInt(string line) => GetInts(line).Single();
 } 
