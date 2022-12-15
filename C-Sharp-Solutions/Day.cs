c479671de9162cc4c1c0971c22042dba7464d160
seanmul10 <36199832+smuldoon1@users.noreply.github.com>
2022-12-15 18:49:31 +0000

    Benchmark each day and part

--- C-Sharp-Solutions/Day.cs
+++ C-Sharp-Solutions/Day.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,17 +13,22 @@ public abstract class Day
     {
         InputPath = inputPath;
         string[] input = File.ReadAllLines(@$"F:\Advent-Of-Code-2022\C-Sharp-Solutions\{InputPath}.txt");
+        Stopwatch stopwatch = new Stopwatch();
+        stopwatch.Start();
         string day1 = PartOne(input);
+        TimeSpan time1 = stopwatch.Elapsed;
         string day2 = PartTwo(input);
-        PrintResult(day1, day2);
+        TimeSpan time2 = stopwatch.Elapsed;
+        stopwatch.Stop();
+        PrintResult(day1, day2, time1, time2);
     }
 
     public abstract string PartOne(string[] input);
 
     public abstract string PartTwo(string[] input);
 
-    public void PrintResult(string answer1, string answer2)
+    public void PrintResult(string answer1, string answer2, TimeSpan time1, TimeSpan time2)
     {
-        Console.WriteLine($"Solution for {InputPath}\nPart 1: {answer1}\nPart 2: {answer2}\n");
+        Console.WriteLine($"Solution for {InputPath}\nPart 1: {answer1} ({time1.Seconds}s)\nPart 2: {answer2} ({time2.Seconds}s)\n");
     }
 }
