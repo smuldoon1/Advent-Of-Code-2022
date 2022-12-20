2855cf94ca658a588caa8ef7dc73aa27bbf8da41
seanmul10 <36199832+smuldoon1@users.noreply.github.com>
2022-12-20 16:37:17 +0000

    Day 20

---
+++ C-Sharp-Solutions/Day20.cs
@@ -0,0 +1,60 @@
+﻿// Part 1 - 
+// Part 2 - 
+
+class Day20 : Day
+{
+    public Day20(string inputPath, bool continueOnError = false) : base(inputPath, continueOnError)
+    {
+    }
+
+    public override string PartOne(string[] input)
+    {
+        int listCount = input.Length;
+        var numList = new List<int>();
+        foreach (var num in input)
+            numList.Add(int.Parse(num));
+
+        var indexList = Enumerable.Range(0, listCount).ToList();
+        for (int i = 0; i < listCount; i++)
+        {
+            var index = indexList.IndexOf(i);
+            int number = numList[index];
+            int newIndex = Utils.Mod(index + numList[index], listCount - 1);
+            numList.RemoveAt(index);
+            numList.Insert(newIndex, number);
+            indexList.RemoveAt(index);
+            indexList.Insert(newIndex, i);
+        }
+        var zeroIndex = numList.IndexOf(0);
+        return (numList[Utils.Mod(zeroIndex + 1000, listCount)] +
+            numList[Utils.Mod(zeroIndex + 2000, listCount)] +
+            numList[Utils.Mod(zeroIndex + 3000, listCount)]).ToString();
+    }
+
+    public override string PartTwo(string[] input)
+    {
+        int listCount = input.Length;
+        var numList = new List<long>();
+        foreach (var num in input)
+            numList.Add(int.Parse(num) * 811589153L);
+
+        var indexList = Enumerable.Range(0, listCount).ToList();
+        for (int x = 0; x < 10; x++)
+        {
+            for (int i = 0; i < listCount; i++)
+            {
+                var index = indexList.IndexOf(i);
+                long number = numList[index];
+                int newIndex = (int)Utils.Mod(index + numList[index], listCount - 1);
+                numList.RemoveAt(index);
+                numList.Insert(newIndex, number);
+                indexList.RemoveAt(index);
+                indexList.Insert(newIndex, i);
+            }
+        }
+        var zeroIndex = numList.IndexOf(0);
+        return (numList[Utils.Mod(zeroIndex + 1000, listCount)] +
+            numList[Utils.Mod(zeroIndex + 2000, listCount)] +
+            numList[Utils.Mod(zeroIndex + 3000, listCount)]).ToString();
+    }
+}
--- C-Sharp-Solutions/Utils.cs
+++ C-Sharp-Solutions/Utils.cs
@@ -4,6 +4,8 @@ public static class Utils
 {
     public static int Mod(int a, int b) => ((a % b) + b) % b;
 
+    public static long Mod(long a, long b) => ((a % b) + b) % b;
+
     public static int[] GetInts(string line) => Regex.Split(line, @"[^0-9-]+").Skip(1).Select(x => int.Parse(x)).ToArray();
 
     public static int GetInt(string line) => GetInts(line).Single();
